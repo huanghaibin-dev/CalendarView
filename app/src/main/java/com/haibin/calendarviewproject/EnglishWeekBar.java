51fb3620bedd8cafd222f48c9681d6189975f286
huanghaibin <huanghaibin_dev@163.com>
2018-06-25 11:12:38 +0800

    update demo

--- app/src/main/java/com/haibin/calendarviewproject/EnglishWeekBar.java
+++ app/src/main/java/com/haibin/calendarviewproject/EnglishWeekBar.java
@@ -3,6 +3,7 @@ package com.haibin.calendarviewproject;
 import android.content.Context;
 import android.graphics.Color;
 import android.view.LayoutInflater;
+import android.widget.TextView;
 
 import com.haibin.calendarview.Calendar;
 import com.haibin.calendarview.WeekBar;
@@ -28,5 +29,35 @@ public class EnglishWeekBar extends WeekBar {
         int viewIndex = getViewIndexByCalendar(calendar, weekStart);
         getChildAt(viewIndex).setSelected(true);
         mPreSelectedIndex = viewIndex;
+    }
+
+    /**
+     * 当周起始发生变化，使用自定义布局需要重写这个方法，避免出问题
+     *
+     * @param weekStart 周起始
+     */
+    @Override
+    protected void onWeekStartChange(int weekStart) {
+        for (int i = 0; i < getChildCount(); i++) {
+            ((TextView) getChildAt(i)).setText(getWeekString(i, weekStart));
+        }
+    }
+
+    /**
+     * 或者周文本，这个方法仅供父类使用
+     * @param index index
+     * @param weekStart weekStart
+     * @return 或者周文本
+     */
+    private String getWeekString(int index, int weekStart) {
+        String[] weeks = getContext().getResources().getStringArray(R.array.english_week_string_array);
+
+        if (weekStart == 1) {
+            return weeks[index];
+        }
+        if (weekStart == 2) {
+            return weeks[index == 6 ? 0 : index + 1];
+        }
+        return weeks[index == 0 ? 6 : index - 1];
     }
 }
